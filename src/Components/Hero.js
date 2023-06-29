ca9b32ec3481778adb2491921e01c2bb0b82dcf3
Ahmed <ahmedjalal0322@gmail.com>
2023-06-29 20:45:36 +0500

    first commit

---
+++ src/Components/Hero.js
@@ -0,0 +1,20 @@
+import half from '../images/main.png'
+import '../Styles/Hero.css'
+import {motion} from "framer-motion"
+
+export default function Hero() {
+    return(
+        <>
+            <div className='Hero'>
+                <button>
+                    <motion.img 
+                        animate={{ scale:[1,.4,1], transform:["rotate(0deg)", "rotate(180deg)", "rotate(360deg)"], filter: ["grayscale(1)", "grayscale(0)", "grayscale(1)"] }}
+                        transition={{duration:5, repeat:Infinity, ease:"linear"}}
+                        src={half}>
+                    </motion.img>
+                </button>
+            </div>
+            <h3>Front End <span className=''>Web</span> Developer</h3>
+        </>
+    )
+}
---
+++ src/Components/Navbar.js
@@ -0,0 +1,22 @@
+import '../Styles/Navbar.css'
+import {motion} from 'framer-motion'
+
+export default function Navbar() {
+    return(
+        <div className="Navbar">
+            <h1>AJ</h1>
+            <ul>
+                <button className='activeNav'>Home</button>
+                <button>Portfolio</button>
+                <button>About Me</button>
+            </ul>
+            <div className='btn'>
+                <motion.button 
+                    whileHover={{backgroundColor:["#fff", "var(--pink)"], color:["var(--pink)", "#fff"], scale:1.2, transition:{duration:.3}}} 
+                    whileTap={{scale:0.9}}>
+                    Get in Touch
+                </motion.button>
+            </div>
+        </div>
+    )
+}
