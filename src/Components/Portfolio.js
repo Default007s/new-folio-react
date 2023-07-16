04ec994c30a528c36af6dd3164ddd8f1d0df0bed
Ahmed <ahmedjalal0322@gmail.com>
2023-07-16 09:28:50 +0500

    portfolio bug fixed

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -21,7 +21,7 @@ export default function Portfolio() {
         
         <div className='port2 port'>
           <motion.img 
-          initial = {{ scale: 1, marginTop: 50,opacity: 0 }} 
+          initial = {{ marginTop: 50,opacity: 0 }} 
           whileInView = {{ margin: 0, opacity: 1, transition:{ duration:1 }}} 
           whileHover={{filter:['grayscale(1)', 'grayscale(0)'], scale:[1, 1.1], zIndex:[0, 1]}} 
           src={jochum}></motion.img>        
