5c773fa6c4d163f3c7bcfe26650481663524f686
VercelDep202 <vercelDep@gmail.com>
2025-02-01 18:48:41 +0500

    about updated

--- src/Components/About.js
+++ src/Components/About.js
@@ -8,7 +8,7 @@ export default function About() {
         <div className='info'>
             <h1>Ahmed Jalal</h1>
             <h2><span className='pinked'>Pakistan</span> based Front End Web Developer focused on <span className='pinked'>React</span></h2>
-            <p>Nostrud reprehenderit eiusmod fugiat ea dolore magna culpa officia reprehenderit nulla non. Pariatur excepteur ad laboris elit incididunt aliqua laborum excepteur. Ad ipsum ipsum nisi anim in laborum cillum. Excepteur nisi consequat cupidatat aliqua ad reprehenderit irure. In sint labore amet sint. Ad irure officia minim sit reprehenderit voluptate quis cupidatat ut laborum ad irure est ad. Magna esse ut aliquip magna non. Do id officia velit ex laboris. Duis nisi sint amet voluptate nisi deserunt id voluptate mollit incididunt consequat.</p>
+            <p>I'm Ahmed Jalal. It is never easy for a young boy to get out of his comfort zone and explore the world. But I am proud that with the help of god I have overtaken this challenge. As a young freelancer, I create websites that generate more clients to help entrepreneurs who want to have a one-of-a-kind social media presence.</p>
             <h2 id='aboutQuote'>If <span className='pinked'>Passion</span> drives you, let <span className='pinked'>Reason</span> hold the reins</h2>
             <a href="mailto:ahmedjalal0322@gmail.com?subject = Feedback">
               <motion.button 
